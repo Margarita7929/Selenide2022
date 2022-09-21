fc9656fe111bc88c82ed0b25a10cd736665d6eeb
Margarita7929 <bykovskayamargarita@gmail.com>
2022-09-21 23:02:44 +0300

    Cahnged test

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
@@ -12,27 +13,36 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
 
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 
 
-        @Test
-        void shouldTest() {
+    @Test
+    void shouldTest() {
 
-            open("http://0.0.0.0:9999");
-            SelenideElement form = $("#root");
-            form.$("[data-test-id=city] input").setValue("Москва");
-            form.$("[data-test-id='date'] input").doubleClick().sendKeys("30.09.2022");
-            form.$("[data-test-id=name] input").setValue("Иванов Иван");
-            form.$("[data-test-id=phone] input").setValue("+79094653782");
-            form.$("[data-test-id=agreement]").click();
-            form.$$("button").find(exactText("Забронировать")).click();
-
-
-            form.$("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15));
-           form.$x(".//div[@class='notification__title']").should(text("Успешно!"));
-            form.$x(".//div[@class='notification__content']").should(text("Встреча успешно забронирована на "));
+        open("http://0.0.0.0:9999");
+        String planningDate = generateDate(3);
+        SelenideElement form = $("#root");
+        form.$("[data-test-id=city] input").setValue("Москва");
+        form.$("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id='date'] input").setValue(planningDate);
+        form.$("[data-test-id=name] input").setValue("Иванов Иван");
+        form.$("[data-test-id=phone] input").setValue("+79094653782");
+        form.$("[data-test-id=agreement]").click();
+        form.$$("button").find(exactText("Забронировать")).click();
 
 
-    }}
+        form.$("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+
+
+    }
+
+
+}
 
 
 
